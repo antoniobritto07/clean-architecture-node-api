0dc96c770c83eb7c68a159048888112cbc05bddb
Antonio <antonio.britto.2000@hotmmail.com>
2024-12-07 22:44:45 -0300

    chore: add eslint and prettier and husky and lint staged

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import globals from "globals"
+import pluginJs from "@eslint/js"
+import tseslint from "@typescript-eslint/eslint-plugin"
+import tsParser from "@typescript-eslint/parser"
+import prettierPlugin from "eslint-plugin-prettier"
+
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: {
+      parser: tsParser,
+      globals: globals.browser,
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...pluginJs.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+      "prettier/prettier": "error",
+    },
+  },
+]
