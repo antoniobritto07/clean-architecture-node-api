320b313a8765995d80bcf53f01c045b97c2e8c62
Antonio <antonio.britto.2000@hotmmail.com>
2024-12-15 15:16:01 -0300

    feat: ensure EmailValidatorAdapter returns false if validator returns false

---
+++ src/utils/email-validator-adapter.spec.ts
@@ -0,0 +1,11 @@
+import { describe, expect, test, jest } from "@jest/globals"
+import { EmailValidatorAdapter } from "./email-validator"
+
+describe("EmailValidator Adapter", () => {
+  test("should return false if validator returns false", () => {
+    const sut = new EmailValidatorAdapter()
+    const isValid = sut.isValid("invalid_email@email.com")
+
+    expect(isValid).toBe(false)
+  })
+})
