53fc335ee24a1d6b5adc68029bc7e1c2ea6dafd6
Antonio <antonio.britto.2000@hotmmail.com>
2025-01-11 10:33:09 -0300

    feat: add SignUp Factory

---
+++ src/main/factories/signup.ts
@@ -0,0 +1,17 @@
+import { SignUpController } from "../../presentation/controllers/signup/signup"
+import { EmailValidatorAdapter } from "../../utils/email-validator-adapter"
+import { DbAddAccount } from "../../data/usecases/add-account/db-add-account"
+import { BcryptAdapter } from "../../infra/criptography/bcrypt-adapter"
+import { AccountMongoRepository } from "./../../infra/db/mongodb/account-repository/account"
+
+// that's the reason why factories are so important, given that its build seems to be a puzzle, given that there are many dependencies among the classes
+// and because we're using small components which have small implementations... Also becasue we're respecting the Solid's Single Principle.
+
+export const makeSignUpController = (): SignUpController => {
+  const salt = 12
+  const emailValidatorAdapter = new EmailValidatorAdapter()
+  const bcryptAdapter = new BcryptAdapter(salt)
+  const accountMongoRepository = new AccountMongoRepository()
+  const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
+  return new SignUpController(emailValidatorAdapter, dbAddAccount)
+}
