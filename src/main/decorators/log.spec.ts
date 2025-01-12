300aeaa2e4cf941294a83e2db08b39a6ff72219a
Antonio <antonio.britto.2000@hotmmail.com>
2025-01-12 11:50:51 -0300

    feat/test: ensure LogControllerDecorator calls controller with correct value

---
+++ src/main/decorators/log.spec.ts
@@ -0,0 +1,37 @@
+import { describe, test, expect, jest } from "@jest/globals"
+import { LogControllerDecorator } from "./log"
+import {
+  Controller,
+  HttpRequest,
+  HttpResponse,
+} from "../../presentation/protocols"
+
+describe("Log Controller Decorator", () => {
+  test("should call controller handle", async () => {
+    class ControllerStub implements Controller {
+      async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+        const httpResponse = {
+          statusCode: 200,
+          body: {
+            name: "Antonio",
+          },
+        }
+        return new Promise((resolve) => resolve(httpResponse))
+      }
+    }
+    const controllerStub = new ControllerStub()
+    const handleSpy = jest.spyOn(controllerStub, "handle")
+    const sut = new LogControllerDecorator(controllerStub)
+    const httpRequest = {
+      body: {
+        email: "any_mail@example.com",
+        name: "any_name",
+        password: "any_password",
+        passwordConfirmation: "any_password",
+      },
+    }
+
+    await sut.handle(httpRequest)
+    expect(handleSpy).toHaveBeenCalledWith(httpRequest)
+  })
+})
--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -3,15 +3,20 @@ import { EmailValidatorAdapter } from "../../utils/email-validator-adapter"
 import { DbAddAccount } from "../../data/usecases/add-account/db-add-account"
 import { BcryptAdapter } from "../../infra/criptography/bcrypt-adapter"
 import { AccountMongoRepository } from "./../../infra/db/mongodb/account-repository/account"
+import { Controller } from "../../presentation/protocols"
+import { LogControllerDecorator } from "../decorators/log"
 
 // that's the reason why factories are so important, given that its build seems to be a puzzle, given that there are many dependencies among the classes
 // and because we're using small components which have small implementations... Also becasue we're respecting the Solid's Single Principle.
-
-export const makeSignUpController = (): SignUpController => {
+export const makeSignUpController = (): Controller => {
   const salt = 12
   const emailValidatorAdapter = new EmailValidatorAdapter()
   const bcryptAdapter = new BcryptAdapter(salt)
   const accountMongoRepository = new AccountMongoRepository()
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
-  return new SignUpController(emailValidatorAdapter, dbAddAccount)
+  const signUpController = new SignUpController(
+    emailValidatorAdapter,
+    dbAddAccount,
+  )
+  return new LogControllerDecorator(signUpController)
 }
