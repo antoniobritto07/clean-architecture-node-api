8342dd05a4179b5d5c348d52acdb332c965b9648
Antonio <antonio.britto.2000@hotmmail.com>
2025-01-31 14:36:07 -0300

    refactor: move password comparation to the ValidationComposite

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -81,22 +81,6 @@ const makeFakeRequest = (): HttpRequest => ({
 })
 
 describe("SignUp Controller", () => {
-  test("should return 400 if an invalid email is provided", async () => {
-    const { sut, emailValidatorStub } = makeSut()
-    //manually mocking the stub default return value to force it fail, and then test the functionality
-    jest.spyOn(emailValidatorStub, "isValid").mockReturnValueOnce(false)
-    const httpRequest = {
-      body: {
-        name: "any_name",
-        email: "invalid_email@email.com",
-        password: "any_password",
-        passwordConfirmation: "any_password",
-      },
-    }
-    const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse).toEqual(badRequest(new InvalidParamError("email")))
-  })
-
   test("should call EmailValidator with correct email", async () => {
     const { sut, emailValidatorStub } = makeSut()
     const isValidSpy = jest.spyOn(emailValidatorStub, "isValid")
@@ -112,23 +96,6 @@ describe("SignUp Controller", () => {
     })
     const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse).toEqual(serverError(new ServerError(null)))
-  })
-
-  test("should return 400 if password confirmation fails", async () => {
-    const { sut, emailValidatorStub } = makeSut()
-    jest.spyOn(emailValidatorStub, "isValid").mockReturnValueOnce(false)
-    const httpRequest = {
-      body: {
-        name: "any_name",
-        email: "any_email@email.com",
-        password: "any_password",
-        passwordConfirmation: "invalid_password",
-      },
-    }
-    const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse).toEqual(
-      badRequest(new InvalidParamError("passwordConfirmation")),
-    )
   })
 
   test("should call AddAccount with correct values", async () => {
