19b761fd699fc31429996b08369fcaf29805a46f
Antonio <antonio.britto.2000@hotmmail.com>
2025-02-03 10:04:15 -0300

    feat/test: ensure AccountMongoRepository returns an account when LoadByEmail success

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -8,7 +8,9 @@ import {
   beforeEach,
 } from "@jest/globals"
 import { AccountMongoRepository } from "./account"
+import { Collection } from "mongodb"
 
+let accountCollection: Collection
 describe("Account Mongo Repository", () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
@@ -19,7 +21,7 @@ describe("Account Mongo Repository", () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection("accounts")
+    accountCollection = await MongoHelper.getCollection("accounts")
     await accountCollection.deleteMany({})
   })
 
@@ -28,7 +30,7 @@ describe("Account Mongo Repository", () => {
   }
 
   //when dealing with integration tests we don't have the freedom that we were used to have, to test exceptions and other different behaviors.
-  test("should return an if of the account created on success", async () => {
+  test("should return an account on add success", async () => {
     const sut = makeSut()
     const account = await sut.add({
       name: "any_name",
@@ -39,5 +41,24 @@ describe("Account Mongo Repository", () => {
 
     expect(account).toBeTruthy()
     expect(account.id).toBeTruthy()
+  })
+
+  test("should return an account on loadByEmail success", async () => {
+    const sut = makeSut()
+    await accountCollection.insertOne({
+      name: "any_name",
+      email: "any_email@mail.com",
+      password: "any_password",
+    })
+    const account = await sut.loadByEmail("any_email@mail.com")
+
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+  })
+
+  test("should return null if loadByEmail fails", async () => {
+    const sut = makeSut()
+    const account = await sut.loadByEmail("any_email@mail.com")
+    expect(account).toBeFalsy()
   })
 })
